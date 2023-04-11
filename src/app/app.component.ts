6fefec7c26cc1e4476289d57b89e547ba772d06d
Jackson Leste <jacksongoncalves.leste@gmail.com>
2023-04-10 21:23:19 -0300

    mostrando dados da transferencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  dadosTranferencia:{};
+
+  transfer($event){
+    this.dadosTranferencia = $event
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Output() onNewTransfer = new EventEmitter<any>();
 
-  ngOnInit(): void {
+  valor: number;
+  destino: number;
+
+  ngOnInit(): void {}
+
+  novaTransferencia() {
+    console.log('Nova solicitação!');
+
+    const dadosTranferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.onNewTransfer.emit(dadosTranferencia);
+    this.limpaForm();
   }
 
-  novaTransferencia(){
-    console.log('Nova solicitação!')
+  limpaForm() {
+    this.valor = 0;
+    this.destino = 0;
   }
-
 }
